a280d0548b069235043037596361fefaeaee4ee2
DESKTOP-ASPUCPF\Mette Tøttrup Gade <mtg15@student.aau.dk>
2018-05-03 15:06:58 +0200

    Slette test klassen og rettet UserName til Username

--- Timmy/app/src/main/java/com/example/gr6402/timmy/MainActivity.java
+++ Timmy/app/src/main/java/com/example/gr6402/timmy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gr6402.timmy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UsernameEt = (EditText)findViewById(R.id.etUserName);
+        UsernameEt = (EditText)findViewById(R.id.etUsername);
         PasswordEt = (EditText)findViewById(R.id.etPassword);
 
     }
--- Timmy/app/src/main/java/com/example/gr6402/timmy/TestClass.java
+++
@@ -1,6 +0,0 @@
-package com.example.gr6402.timmy;
-
-public class TestClass {
-
-    //Her sker der et eller andet fedt
-}
