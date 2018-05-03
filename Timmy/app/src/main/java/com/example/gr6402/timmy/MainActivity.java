fb7162a590f233d5f577c9a1a4fb9812fb495bcb
Bo Hansen <bwha15@student.aau.dk>
2018-05-03 13:46:21 +0200

    Lavet indledende knapper til brugerlogin

--- Timmy/app/src/main/java/com/example/gr6402/timmy/MainActivity.java
+++ Timmy/app/src/main/java/com/example/gr6402/timmy/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.gr6402.timmy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText UsernameEt, PasswordEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UsernameEt = (EditText)findViewById(R.id.etUserName);
+        PasswordEt = (EditText)findViewById(R.id.etPassword);
+
     }
+}
+
+
+public void OnLogin(View view) {
+    
 }
