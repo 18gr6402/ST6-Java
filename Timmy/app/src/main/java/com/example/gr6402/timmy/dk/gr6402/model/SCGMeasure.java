19a08d42c9e97b26e1f08073f35dc292770b9be1
Steffen Jensen <sjens15@student.aau.dk>
2018-05-15 16:59:23 +0200

    SCGMeasure gjort parcelable

--- Timmy/app/src/main/java/com/example/gr6402/timmy/dk/gr6402/model/SCGMeasure.java
+++ Timmy/app/src/main/java/com/example/gr6402/timmy/dk/gr6402/model/SCGMeasure.java
@@ -1,11 +1,13 @@
 package com.example.gr6402.timmy.dk.gr6402.model;
 
-public class SCGMeasure {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class SCGMeasure implements Parcelable{
 
     // attributes
-
     private Object date;
-    private Integer fiducialmarkers;
+    private String fiducialmarkers;
     private String scg;
     private Integer mpi;
     private Integer weight;
@@ -14,94 +16,153 @@ public class SCGMeasure {
     private String fatigue;
     private Boolean warning;
 
-    // methods
 
-    public void scgMeasure(){
-
+    //constructors
+    public SCGMeasure() {
     }
 
-    public void setDate(){
-
+    public SCGMeasure(Object date, String fiducialmarkers, String scg, int mpi, int weight, String angina, String dyspnea, String fatigue, Boolean warning){
+        this.date = date;
+        this.fiducialmarkers = fiducialmarkers;
+        this.scg = scg;
+        this.mpi = mpi;
+        this.weight = weight;
+        this.angina = angina;
+        this.dyspnea = dyspnea;
+        this.fatigue = fatigue;
+        this.warning = warning;
     }
 
-    public void getDate(){
 
+    // set and get methods
+    public Object getDate() {
+        return date;
     }
 
-    public void getFiducialMarkers(){
-        // int
+    public void setDate(Object date) {
+        this.date = date;
     }
 
-    public void setSCG(String scg){
-
+    public String getFiducialmarkers() {
+        return fiducialmarkers;
     }
 
-    public void getSCG(){
-        // String
+    public void setFiducialmarkers(String fiducialmarkers) {
+        this.fiducialmarkers = fiducialmarkers;
     }
 
-    public void scgProperty(){
-        // String
+    public String getScg() {
+        return scg;
     }
 
-    public void getMPI(){
-        // int
+    public void setScg(String scg) {
+        this.scg = scg;
     }
 
-    public void setWeight(int weight){
-
+    public Integer getMpi() {
+        return mpi;
     }
 
-    public void getWeight(){
-        // int
+    public void setMpi(Integer mpi) {
+        this.mpi = mpi;
     }
 
-    public void weightProperty(){
-        // Integer
+    public Integer getWeight() {
+        return weight;
     }
 
-    public void setAngina(String angina){
-
+    public void setWeight(Integer weight) {
+        this.weight = weight;
     }
 
-    public void getAngina(){
-        // String
+    public String getAngina() {
+        return angina;
     }
 
-    public void anginaProperty(){
-        // String
+    public void setAngina(String angina) {
+        this.angina = angina;
     }
 
-    public void setDyspnea(String dyspnea){
-
+    public String getDyspnea() {
+        return dyspnea;
     }
 
-    public void getDyspnea(){
-        // String
+    public void setDyspnea(String dyspnea) {
+        this.dyspnea = dyspnea;
     }
 
-    public void dyspneaProperty(){
-        // String
+    public String getFatigue() {
+        return fatigue;
     }
 
-    public void setFatigue(String fatigue){
-
+    public void setFatigue(String fatigue) {
+        this.fatigue = fatigue;
     }
 
-    public void getFatigue(){
-        // String
+    public Boolean getWarning() {
+        return warning;
     }
 
-    public void fatiguePropety(){
-        // String
+    public void setWarning(Boolean warning) {
+        this.warning = warning;
     }
 
-    public void getWarning(){
-        // boolean
+
+    // parcelable part
+    protected SCGMeasure(Parcel in) {
+        date = (Object) in.readValue(Object.class.getClassLoader());
+        fiducialmarkers = in.readString();
+        scg = in.readString();
+        mpi = in.readByte() == 0x00 ? null : in.readInt();
+        weight = in.readByte() == 0x00 ? null : in.readInt();
+        angina = in.readString();
+        dyspnea = in.readString();
+        fatigue = in.readString();
+        warning = in.readByte() != 0;
     }
 
-    public void warningProperty(){
-        // Boolean
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(date);
+        dest.writeString(fiducialmarkers);
+        dest.writeString(scg);
+        if (mpi == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(mpi);
+        }
+        if (weight == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(weight);
+        }
+        dest.writeString(angina);
+        dest.writeString(dyspnea);
+        dest.writeString(fatigue);
+        if (warning == null) {
+            dest.writeByte((byte) (0x02));
+        } else {
+            dest.writeByte((byte) (warning ? 0x01 : 0x00));
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<SCGMeasure> CREATOR = new Parcelable.Creator<SCGMeasure>() {
+        @Override
+        public SCGMeasure createFromParcel(Parcel in) {
+            return new SCGMeasure(in);
+        }
+
+        @Override
+        public SCGMeasure[] newArray(int size) {
+            return new SCGMeasure[size];
+        }
+    };
 }
