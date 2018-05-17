a19d2078f46b88aa021742bcc13bbe3a08eb425e
Mette Tøttrup Gade <mtg15@student.aau.dk>
2018-05-17 10:10:12 +0200

    tilføjet drop down i login

--- Timmy/app/src/main/java/com/example/gr6402/timmy/dk/gr6402/controller/LoginCtrl.java
+++ Timmy/app/src/main/java/com/example/gr6402/timmy/dk/gr6402/controller/LoginCtrl.java
@@ -1,12 +1,12 @@
 package com.example.gr6402.timmy.dk.gr6402.controller;
 
-import android.app.Activity;
-import android.arch.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -14,23 +14,36 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.gr6402.timmy.BackgroundWorker;
 import com.example.gr6402.timmy.R;
 import com.example.gr6402.timmy.dk.gr6402.model.Patient;
 import com.example.gr6402.timmy.dk.gr6402.model.Practitioner;
-import com.example.gr6402.timmy.dk.gr6402.model.User;
 import com.example.gr6402.timmy.dk.gr6402.myinterface.DatabaseOperations;
 import com.example.gr6402.timmy.dk.gr6402.myinterface.DatabaseTask;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-public class LoginCtrl extends AppCompatActivity implements DatabaseOperations{
+
+public class LoginCtrl extends AppCompatActivity implements DatabaseOperations, AdapterView.OnItemSelectedListener {
     // objekter i viewet defineres her som attributter, med type og id på objektet (vores eget valg).
     public CheckBox cbCheckPractitioner;
-    public TextView clinincText;
-    public Spinner clinincSpinner;
-    public EditText userNameField, passwordField;
+    public TextView clinicTxt;
+    public TextView userNameTxt;
+    public EditText userNameField;
+    public TextView passwordTxt;
+    public EditText passwordField;
     private Button btnLogin;
     private Button btnNewUser;
+
+    // This is the declaration part for the spinner
+    private InputStream is = null;
+    private String result = null;
+    private String line = null;
+
+    private String[] name;
+
+    public Spinner clinicSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,26 +52,62 @@ public class LoginCtrl extends AppCompatActivity implements DatabaseOperations{
 
         // forbinder java klassen med objektet i viewet
         cbCheckPractitioner = (CheckBox) findViewById(R.id.cbCheckPractitioner);
-        clinincText = (TextView) findViewById(R.id.clinicText);
-        clinincSpinner = (Spinner) findViewById(R.id.clinincSpinner);
+        clinicTxt = (TextView) findViewById(R.id.clinicTxt);
+        clinicSpinner = (Spinner) findViewById(R.id.clinicSpinner); //Here we define the spinner
         userNameField = (EditText) findViewById(R.id.userNameField);
         passwordField = (EditText) findViewById(R.id.passwordField);
         btnNewUser = (Button) findViewById(R.id.btnNewUser);
         btnLogin = (Button) findViewById(R.id.btnLogin);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void clinicSpinner(String[] separated) {
+        // Her forsøges at lave en spinner med dummy data
+        clinicSpinner.setOnItemSelectedListener(this); // Spinner click listener
+
+        List<String> clinics = new ArrayList<String>();  // Spinner Drop down elements
+        clinics.add("Vælg klinik");
+        for (int i=0; i<separated.length; i+=2){
+           clinics.add(separated[i]);
+        }
+
+        // Creating adapter for spinner, som henter data fra vores clinic array
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, clinics);
+
+        // Drop down layout style - list view with radio button
+        // dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        clinicSpinner.setAdapter(dataAdapter);
+    }
+
+
 /*
-Ændre på vinduet (syndligheden af clinic dropdown og standart indholdet i brugernavn feltet) når der
+Ændre på vinduet (syndligheden af clinic dropdown og standard indholdet i brugernavn feltet) når der
  klikkes på checkboksen cbCheckPractitioner
  */
     public void checkPractitioner(View view){
         if (cbCheckPractitioner.isChecked()) {
-            clinincText.setVisibility(View.VISIBLE);
-            clinincSpinner.setVisibility(View.VISIBLE);
+            clinicTxt.setVisibility(View.VISIBLE);
+            clinicSpinner.setVisibility(View.VISIBLE);
             userNameField.setText("Ansættelses ID");
+            String type = "loadClinics";
+            DatabaseTask databaseTask = new DatabaseTask(this);
+            databaseTask.execute(type);
         }else {
-            clinincText.setVisibility(View.GONE);
-            clinincSpinner.setVisibility(View.GONE);
+            clinicTxt.setVisibility(View.GONE);
+            clinicSpinner.setVisibility(View.GONE);
             userNameField.setText("CPR");
         }
     }
@@ -88,14 +137,13 @@ login. Udføre den tilhørende handling for intent af næste menu vindue.
             DatabaseTask databaseTask = new DatabaseTask(this);
             databaseTask.execute(type,userName,password);
         }
-
     }
 
 /*
 Åbner vinduet for registrering af ny bruger.
  */
     public void handleNewUser(View view){
-        Intent in_newview = new Intent(this, NewCtrl.class);
+        Intent in_newview = new Intent(this, NewUserCtrl.class);
         startActivity(in_newview);
     }
 
@@ -120,6 +168,10 @@ login. Udføre den tilhørende handling for intent af næste menu vindue.
         }
         else if (output.equals("FALSE")){
             Toast.makeText(this, "Forkert loginoplysninger", Toast.LENGTH_LONG).show();
+        }
+        else {
+            String[] separated = output.split(",");
+            clinicSpinner(separated);
         }
     }
 
